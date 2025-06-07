ef91d2534c5c7a51bcaf35eb34b07adef255d84c
Diodes <bbatsios@yahoo.ca>
2025-06-07 05:00:21 -0400

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
-// db.js
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Needed for Render
+  },
 });
 
 module.exports = pool;
